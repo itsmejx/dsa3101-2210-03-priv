d0b88492583b366917fa19047855af511fd3519c
siying01 <siying.lim.cf2001@gmail.com>
2022-11-12 18:02:17 +0800

    edited api.py to include additional requests route

--- backend/api.py
+++ backend/api.py
@@ -14,5 +14,18 @@ def get_date():
     data = json.loads(f.read())
     return data
 
+@app.route("/get_dates", methods = ["GET"])
+def get_dates():
+    new_data = request.get_json()
+    dates = new_data.get('dates')
+    data = []
+    for date in dates:
+        json_file = f"{date}.json"
+        filename = os.path.join('DB', json_file)
+        f = open(filename, "r")
+        data_tmp = json.loads(f.read())
+        data.append(data_tmp)
+    return data
+
 if __name__ == "__main__":
     app.run()
